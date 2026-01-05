842f4dcb4ecb5355cab16e9eaee1a49e76b716cd
dachraben-png <dachraben@gmail.com>
2026-01-05 19:42:54 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const siteConfig = {
+  "siteName": "Coffee Egan",
+  "logoUrl": "logo.png",
+  "telegramUsername": "werebeu92",
+  "miniAppUrl": "https://t.me/ton_miniapp",
+  "instagramUrl": "https://instagram.com/ton_compte",
+  "canalUrl": "https://t.me/ton_canal",
+  "contactUsername": "werebeu92",
+  "infoTitle": "À propos de Coffee Egan",
+  "infoMainText": "Bienvenue sur notre shop ! Retrouvez nos meilleurs produits sélectionnés avec soin.",
+  "infoDeliveryTitle": "📦 Livraison",
+  "infoDeliveryText": "Livraison rapide et discrète partout en France. Délai de 24-48h.",
+  "infoPaymentTitle": "💳 Paiement",
+  "infoPaymentText": "Modes de paiement sécurisés disponibles : Crypto, espèces.",
+  "categories": [
+    "herbes",
+    "hash",
+    "concentre"
+  ]
+};
